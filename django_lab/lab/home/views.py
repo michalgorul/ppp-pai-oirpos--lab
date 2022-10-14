c4cf4ce71d1eeed6a12edd67e51cfec6fef6ded2
Michał Góral <michalgorul@gmail.com>
2022-10-14 20:57:42 +0200

    update master branch

--- django_lab/lab/home/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-from django.http import HttpResponse, HttpRequest
-
-
-def index(request: HttpRequest) -> HttpResponse:
-    return render(request, "index.html")
