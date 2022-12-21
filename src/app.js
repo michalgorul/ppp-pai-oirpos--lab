e546476c5655edb0e013daf44a17abb7ae03ee17
michalgorul <michalgorul@gmail.com>
2022-12-21 22:31:50 +0100

    format

--- src/app.js
+++ src/app.js
@@ -1,6 +1,6 @@
 // noinspection JSCheckFunctionSignatures
 const WebSocket = require('ws');
-const { WebSocketServer } = require('ws');
+const {WebSocketServer} = require('ws');
 
 const express = require('express');
 const cors = require('cors');
@@ -13,18 +13,18 @@ const server = require('http').createServer(app);
 app.use(express.static(`${__dirname}/websocket/`));
 
 app.use(cors({
-  origin: 'http://localhost:3000',
-  methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
-  credentials: true,
-  optionsSuccessStatus: 200,
+    origin: 'http://localhost:3000',
+    methods: ['POST', 'PUT', 'GET', 'OPTIONS', 'HEAD'],
+    credentials: true,
+    optionsSuccessStatus: 200,
 }));
 
 app.use(express.json());
 
 const sessionParser = session({
-  saveUninitialized: false,
-  secret: '$secret',
-  resave: false,
+    saveUninitialized: false,
+    secret: '$secret',
+    resave: false,
 });
 
 app.use(sessionParser);
@@ -38,49 +38,58 @@ app.use('/api/test-get', (req, res) => res.send('testGet working'));
 app.use('/api/users', usersRouter);
 app.use('/api/messages', messagesRouter);
 
-const wss = new WebSocketServer({ port: 8081 });
+const wss = new WebSocketServer({
+    port: 8081
+});
 
 server.on('upgrade', (request, socket, head) => {
-  // Sprawdzenie czy dla danego połączenia istnieje sesja
-  sessionParser(request, {}, () => {
-    if (!request.session.userId) {
-      socket.destroy();
-      return;
-    }
-    wss.handleUpgrade(request, socket, head, (ws) => {
-      wss.emit('connection', ws, request);
+    // Sprawdzenie czy dla danego połączenia istnieje sesja
+    sessionParser(request, {}, () => {
+        if (!request.session.userId) {
+            socket.destroy();
+            return;
+        }
+        wss.handleUpgrade(request, socket, head, (ws) => {
+            wss.emit('connection', ws, request);
+        });
     });
-  });
 });
 
 const onlineUsers = {};
 
 wss.on('connection', (ws, request) => {
-  wss.clients.forEach((client) => {
-    if (client.readyState === WebSocket.OPEN) {
-      client.send(JSON.stringify({ status: 2 }));
-    }
-  });
-  try {
-    onlineUsers[request.session.userId] = ws;
-  } catch (err) {
-    console.log(err);
-  }
-  console.log('A new client connected');
-
-  ws.on('message', (message) => {
-    console.log(String(message));
-    // parsowanie wiadomosci z JSONa na obiekt
+    wss.clients.forEach((client) => {
+        if (client.readyState === WebSocket.OPEN) {
+            client.send(JSON.stringify({status: 2}));
+        }
+    });
     try {
-      const data = JSON.parse(message);
-    } catch (e) {
-      // console.log(e);
-    }
-  });
+        console.log('request.session')
+        console.log(request.session)
+        if (request.session.userId !== undefined)
+            onlineUsers[request.session.userId] = ws;
+    } catch (err) {
 
-  ws.on('close', () => {
-    delete onlineUsers[request.session.userId];
-  });
+    }
+    console.log('A new client connected');
+
+    ws.on('message', (message) => {
+        console.log(String(message));
+        // parsowanie wiadomosci z JSONa na obiekt
+        try {
+            const data = JSON.parse(message);
+        } catch (e) {
+            // console.log(e);
+        }
+    });
+
+    ws.on('close', () => {
+        try {
+            delete onlineUsers[request.session.userId];
+        } catch (e) {
+            // console.log(e);
+        }
+    });
 });
 
 server.listen(PORT, () => console.log('Server started'));
@@ -88,7 +97,7 @@ server.listen(PORT, () => console.log('Server started'));
 const users = () => onlineUsers;
 
 module.exports = {
-  app, server, sessionParser,
+    app, server, sessionParser,
 };
 
 exports.users = users;
